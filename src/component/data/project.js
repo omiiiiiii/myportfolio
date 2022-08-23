2268d1894cd16a13abc8adf85a0b14319782cee9
omiiiiiii <dwivedi156om@gmail.com>
2022-08-23 07:04:00 -0700

    first commit

--- src/component/data/project.js
+++ src/component/data/project.js
@@ -2,7 +2,7 @@ export const ProjectData=[
     {
         id:1,
         title:"Event-Management",
-        about:"This program used to manage the Event thorugh different stages ,After confirmation user can register on the perticular Event",
+        about:"This program used to manage the Event through different stages ,After confirmation user can register on the particular Event",
         tags:["NodeJS","MongoDB","JavaScript","ExpressJS"],
         demo:"https://event-app-om.herokuapp.com/",
         github:"https://github.com/omiiiiiii/event.github.io",
@@ -10,8 +10,8 @@ export const ProjectData=[
     },
     {
         id:2,
-        title:"Zomatao-Clone",
-        about:"This is standered Zomatao-clone using built on ReactJS javascript",
+        title:"Zomato-Clone",
+        about:"This is standard Zomato-clone using built on ReactJS javascript",
         tags:["ReactJS","JavaScript","Html/CSS"],
         demo:"https://omiiiiiii.github.io/xomatao-clone-live/",
         github:"https://github.com/omiiiiiii/xomatao-clone-live",
