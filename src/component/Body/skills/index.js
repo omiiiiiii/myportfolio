71f6dea3c7383b8e6de2fcab3aa3afcf7f5377ce
omiiiiiii <dwivedi156om@gmail.com>
2022-08-26 09:57:55 -0700

    animation  commit

--- src/component/Body/skills/index.js
+++ src/component/Body/skills/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Seprator from '../../common/socialcontact/seprator'
 import { Skilldata } from '../../data/skill'
 import Skillcard from './skill-card';
@@ -7,9 +7,37 @@ import './skill.css'
 
 function Skills() {
     const data = Skilldata;
+    const toggleAnimation=(e)=>{
+        if(e[0].isIntersecting){
+            setshowAnimation(true)
+        }   
+    }
+    const option={
+        root:null,
+        rootMargin:"0px",
+        threshold:0.5
+    }
+    const [showAnimation ,setshowAnimation]=useState(false)
+    const ref =useRef(null)
+    useEffect(() => {
+        const observer=new IntersectionObserver(toggleAnimation,option)
+        if(!showAnimation){
+            if(ref.current){
+                observer.observe(ref.current)
+            }
+        }
+        
+        return()=>{
+            
+            if(ref.current){
+                // eslint-disable-next-line
+                observer.unobserve(ref.current)
+            }
+        }
+    })
   return (
   
-    <div className='skills'>
+    <div className={`skills ${showAnimation?'swirl-in-fwd':""}`} ref={ref}>
         <Seprator />
         <label className='section-title'>Skills</label>
         <div className='skill-container'>
